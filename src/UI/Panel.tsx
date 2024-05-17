670eb4ef62c0f0898101c4fed519d4f016a75a60
Fernando Badin <fbadin@gmail.com>
2024-05-17 10:31:16 -0400

    added zero state and updated typescript

--- src/UI/Panel.tsx
+++ src/UI/Panel.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 
 type Props = {
-	children: React.ReactNode
+	children: React.ReactNode;
+	className?: string;
 }
 
-const Painel = (props: Props) => {
+const Painel = ({ children, className }: Props) => {
+	const customClassName = `${className ? className : ''}`;
+
 	return (
-		<div className="rounded-md text-dark-1 bg-dark-grey p-3">
-			{props.children}
+		<div className={`rounded-md text-dark-1 bg-dark-grey p-3 ${customClassName}`}>
+			{children}
 		</div>
 	)
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { People } from 'react-bootstrap-icons';
 
 import { EmployeesData, fetchEmployees } from '../api/employees';
 import { AppContext } from '../contexts/appContext';
 import { URLS } from '../routes';
+import { Painel } from '../UI/Panel';
 
 const Dashboard = () => {
   const appContext = React.useContext(AppContext);
@@ -29,26 +31,34 @@ const Dashboard = () => {
     })();
   }, []);
 
+  const showZeroState = employees?.employees.length === 0;
+
   return (
     <div data-testid="main-container">
       <h1 className="text-lg">Employees Dashboard</h1>
       <ListGroup className="mt-3">
         {
-          employees?.employees.map((employee) => {
-            return (
-              <ListGroup.Item
-                action
-                key={employee.id}
-                variant="dark"
-                onClick={() => navigate(URLS.EMPLOYEE_DETAILS(employee.id))}
-              >
-                <div className='flex justify-between'>
-                  {employee.name}
-                  <div className="text-gray-500">{employee.department}</div>
-                </div>
-              </ListGroup.Item>
-            )
-          })
+          showZeroState ? (
+            <Painel className='bg-dark-gray flex justify-center items-center gap-4'>
+              <People /> There are no employees found
+            </Painel>
+          ) : (
+            employees?.employees.map((employee) => {
+              return (
+                <ListGroup.Item
+                  action
+                  key={employee.id}
+                  variant="dark"
+                  onClick={() => navigate(URLS.EMPLOYEE_DETAILS(employee.id))}
+                >
+                  <div className='flex justify-between'>
+                    {employee.name}
+                    <div className="text-gray-500">{employee.department}</div>
+                  </div>
+                </ListGroup.Item>
+              )
+            })
+          )
         }
       </ListGroup>
     </div>
